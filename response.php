0b487b074729e77b7b3078f8a6f8424e511ff9fd
redscoding <109819829+redscoding@users.noreply.github.com>
2023-03-15 00:44:20 +0800

    Add files via upload

---
+++ response.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang = 'en'>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--css-->
+   
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+    
+    
+</head>
+<body>
+    <p class ="center_absolute font_size_massive">MESSAGE SENT !!</P>
+
+</body>
+</html>
