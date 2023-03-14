ddcf21ff7b35ad73313a309b888c5d164849699c
redscoding <darrenchen0908@gmail,com>
2023-03-14 23:53:12 +0800

    first commit

---
+++ PHPEMAIL/PHPEMAIL/js/script.js
@@ -0,0 +1,9 @@
+/*===============typing animation====*/
+
+var typed = new Typed(".typing" , {
+
+    strings:["","SOFTWARE ENGINEERING","WEB DESIGNING ","CODING"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
