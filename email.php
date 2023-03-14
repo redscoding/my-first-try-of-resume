e82e9363f50c0457501a6f41ae77358f238a4f9e
redscoding <109819829+redscoding@users.noreply.github.com>
2023-03-15 02:23:40 +0800

    Update email.php

--- email.php
+++ email.php
@@ -7,12 +7,12 @@ header("Content-Type:text/html; charset=utf-8");
  require 'PHPMailer/src/PHPMailer.php';
  require 'PHPMailer/src/SMTP.php';
 
- if(isset($_GET['submit']))
+ if(isset($_POST['submit']))
  {
-    $name = htmlentities($_GET['name']);
-    $email = htmlentities($_GET['email']);
-    $subject = htmlentities($_GET['subject']);
-    $message = htmlentities($_GET['message']);
+    $name = htmlentities($_POST['name']);
+    $email = htmlentities($_POST['email']);
+    $subject = htmlentities($_POST['subject']);
+    $message = htmlentities($_POST['message']);
 
     $mail = new PHPMailer(true);
     $mail->isSMTP();
