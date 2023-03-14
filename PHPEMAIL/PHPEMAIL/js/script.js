eec51c0d8d240f285f137d86e3c69d9a258de479
redscoding <109819829+redscoding@users.noreply.github.com>
2023-03-15 00:33:15 +0800

    Delete PHPEMAIL/PHPEMAIL directory

--- PHPEMAIL/PHPEMAIL/js/script.js
+++
@@ -1,9 +0,0 @@
-/*===============typing animation====*/
-
-var typed = new Typed(".typing" , {
-
-    strings:["","SOFTWARE ENGINEERING","WEB DESIGNING ","CODING"],
-    typeSpeed:100,
-    BackSpeed:60,
-    loop:true
-})
